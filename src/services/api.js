f1e1edc8c0c903802c5f3da950ec1292b55eeff7
Willy <willyca97@gmail.com>
2025-09-10 15:25:00 +0200

    changed base url to production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://uebungsprojekt-maria-db.vercel.app/api"
+    : "http://localhost:3000/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL,
 });
 
 export default api;
